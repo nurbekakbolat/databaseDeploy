18c67309a6b87928aff6de590487730d0647b776
Nurbek <nurbekakbolat9@gmail.com>
2022-11-21 21:26:09 +0600

    asd

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested, Content-Type, Accept Authorization"
+//   );
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
 const db = mysql.createConnection({
   host: "eu-cdbr-west-03.cleardb.net",

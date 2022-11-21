41e7ff0093acebad98fe0a2d2a04bce2b7649b75
Nurbek <nurbekakbolat9@gmail.com>
2022-11-21 21:23:04 +0600

    asd

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import mysql from "mysql";
 import cors from "cors";
+const cors = require("cors");
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");

58b839f5d237d1b6198ca271abeb284b3583e922
Nurbek <nurbekakbolat9@gmail.com>
2022-11-21 21:57:10 +0600

    qweqw

--- index.js
+++ index.js
@@ -9,19 +9,6 @@ app.use(
   })
 );
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested, Content-Type, Accept Authorization"
-//   );
-//   if (req.method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-//     return res.status(200).json({});
-//   }
-//   next();
-// });
-
 const db = mysql.createConnection({
   host: "eu-cdbr-west-03.cleardb.net",
   user: "bfb0dce41cbf73",
